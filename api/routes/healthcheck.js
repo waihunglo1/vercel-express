50398852bc9b3f13c3c7d5d02283b64be471cc1d
stayinvest-for-dummy <waihunglo@gmail.com>
2025-05-17 21:06:25 +0800

    feat: add health check endpoint with PostgreSQL version and update dependencies

--- api/routes/healthcheck.js
+++ api/routes/healthcheck.js
@@ -1,15 +1,32 @@
 var express = require('express');
 var router = express.Router();
+require('dotenv').config();
+
+const { neon } = require('@neondatabase/serverless');
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+console.log(`Connecting to Neon database at ${PGHOST}...`);
+
+const sql = neon(`postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require`);
+
+async function getPgVersion() {
+  const result = await sql`SELECT version()`;
+  var version = result[0];
+
+  return version;
+}
 
 /**
  * main function
  */
-router.get('/', function (req, res, next) {
-    var hc = {
-        "health": "yes"
-      }
+router.get('/', async function (req, res, next) {
+  var version = await getPgVersion();
 
-    res.json(hc);
+  var hc = {
+    "health": "yes",
+    "version": version
+  }
+
+  res.json(hc);
 });
 
 module.exports = router;
